75af9fc80fa3c2e5e3016ea9514f9e621129f9b9
temcoemail <temcoemail@gmail.com>
2023-03-08 11:41:55 +0800

    publish slidev:银行业务与货币派生 1st time

---
+++ assets/PresenterPrint-edc1b7dc.js
@@ -0,0 +1,17 @@
+import{d,i as _,a as p,u,b as h,c as m,e as f,f as n,g as t,t as s,h as a,F as g,r as v,n as x,j as y,o as l,k as b,l as N,m as k,p as P,q as S,_ as w}from"./index-6e03acef.js";import{N as T}from"./NoteDisplay-6b8f7d13.js";const V={class:"m-4"},j={class:"mb-10"},C={class:"text-4xl font-bold mt-2"},L={class:"opacity-50"},B={class:"text-lg"},D={class:"font-bold flex gap-2"},H={class:"opacity-50"},z=t("div",{class:"flex-auto"},null,-1),F={key:0,class:"border-gray-400/50 mb-8"},M=d({__name:"PresenterPrint",setup(R){_(p),u(`
+@page {
+  size: A4;
+  margin-top: 1.5cm;
+  margin-bottom: 1cm;
+}
+* {
+  -webkit-print-color-adjust: exact;
+}
+html,
+html body,
+html #app,
+html #page-root {
+  height: auto;
+  overflow: auto !important;
+}
+`),h({title:`Notes - ${m.title}`});const i=f(()=>y.slice(0,-1).map(o=>{var r;return(r=o.meta)==null?void 0:r.slide}).filter(o=>o!==void 0&&o.noteHTML!==""));return(o,r)=>(l(),n("div",{id:"page-root",style:x(a(S))},[t("div",V,[t("div",j,[t("h1",C,s(a(m).title),1),t("div",L,s(new Date().toLocaleString()),1)]),(l(!0),n(g,null,v(a(i),(e,c)=>(l(),n("div",{key:c,class:"flex flex-col gap-4 break-inside-avoid-page"},[t("div",null,[t("h2",B,[t("div",D,[t("div",H,s(e==null?void 0:e.no)+"/"+s(a(b)),1),N(" "+s(e==null?void 0:e.title)+" ",1),z])]),k(T,{"note-html":e.noteHTML,class:"max-w-full"},null,8,["note-html"])]),c<a(i).length-1?(l(),n("hr",F)):P("v-if",!0)]))),128))])],4))}}),E=w(M,[["__file","/Users/xumi/NutCloud/=Record=/=Note=/=Slidev=/firstTest/node_modules/@slidev/client/internals/PresenterPrint.vue"]]);export{E as default};
